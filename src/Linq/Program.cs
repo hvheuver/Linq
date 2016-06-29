1b2df965608bf7591125a5dc9c7076ed6e6587d4
ksa607 <karine.samyn@hogent.be>
2016-06-29 11:30:40 +0200

    Create Starter Application

--- src/Linq/Program.cs
+++ src/Linq/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Reflection;
 
 namespace Linq
 {
@@ -9,6 +7,30 @@ namespace Linq
     {
         public static void Main(string[] args)
         {
+            string keuze = "";
+            while (keuze != "99")
+            {
+                Console.WriteLine("\n\n-------------LINQ Menu--------------");
+                Console.WriteLine("1. Step 1: Extension methods.");
+                Console.WriteLine("2. Step 2: Enkele eenvoudige Linq operatoren.");
+                Console.WriteLine("3. Step 3: Where en lambda expressies");
+                Console.WriteLine("4. Step 4: Select en var/anonieme types");
+                Console.WriteLine("5. Step 5: Nog meer handige LINQ operatoren");
+                Console.WriteLine("6. Oefeningen");
+                Console.WriteLine("99. Exit.");
+                Console.Write("Enter your choice : ");
+                keuze = Console.ReadLine();
+                if (keuze != "99")
+                {
+                    Type type = Type.GetType("Linq.Step" + keuze);
+                    if (type != null)
+                    {
+                        Object o = Activator.CreateInstance(type);
+                        type.GetMethod("Execute").Invoke(o, null);
+                    }
+                }
+            }
+
         }
     }
 }
